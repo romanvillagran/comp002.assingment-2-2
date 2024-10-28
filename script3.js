c30a0a0da3438eb8f24b6f27847a05414ba27aa0
Your Name <you@example.com>
2024-10-27 21:25:53 -0700

    added final requirement

--- script3.js
+++ script3.js
@@ -9,3 +9,17 @@
 //  # # # #
 // # # # #
 //  # # # #
+const size = 8;
+
+for (let i = 0; i < size; i++){
+    let row = '';
+
+    for (let j = 0; j < size; j++){
+        if ((i + j) % 2 === 0){
+            row += '#';
+        } else {
+            row += '';
+        }
+    }
+    console.log(row);
+}
